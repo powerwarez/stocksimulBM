ec0c348b069dfd1a13608a5f36a5fb1c35b208ed
powerwarez <powerware@naver.com>
2025-02-28 19:11:04 +0900

    로그인 후 데이터 로드

--- app.py
+++ app.py
@@ -887,12 +887,12 @@ def login_page():
             response = supabase.table('users').select('*').eq('account', account).eq('pw', password).execute()
             if response.data and len(response.data) > 0:
                 st.success('로그인 성공!')
-                user_data = response.data[0].get('data', {})
-                # 기존 user_data를 session_state에 저장하고, 저장된 설정값을 직접 적용
-                st.session_state['user_data'] = user_data
-                st.session_state['account'] = account
-                for key, value in user_data.items():
-                    st.session_state[key] = value
+                user_data = response.data[0].get('data')
+                if user_data is not None:
+                    st.session_state['user_data'] = user_data
+                    st.session_state['account'] = account
+                    for key, value in user_data.items():
+                        st.session_state[key] = value
                 try:
                     if hasattr(st, 'experimental_rerun'):
                         st.experimental_rerun()
